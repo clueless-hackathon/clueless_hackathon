adb7e6292910a9e500c85f712d537dae83ad10e5
Andy <77463134+Andy8154@users.noreply.github.com>
2023-09-30 11:48:40 -0500

    Button works

--- Source/Main.js
+++ Source/Main.js
@@ -1,11 +1,12 @@
 // Get all the buttons and add click event listeners
-const buttons = document.querySelectorAll('.selected');
-
+const buttons = document.querySelectorAll('.button');
+console.log(buttons)
 buttons.forEach(button => {
   button.addEventListener('click', () => {
     // Remove the 'select' class from all buttons
     buttons.forEach(btn => {
       btn.classList.remove('selected');
+      
     });
 
     // Add the 'select' class to the clicked button
