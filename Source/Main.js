41a2a4aa55a5c8f2535b617c791253223871b8f0
Brandon-Ledet <120496007+Brandon-Ledet@users.noreply.github.com>
2023-09-30 13:11:51 -0500

    Revert "Recipe Download WORKING"
    This reverts commit 4d8bb76a5302804d2e2c3a791cc5cb6b940dde99.

--- Source/Main.js
+++ Source/Main.js
@@ -5,10 +5,11 @@ function redirect(){
     window.location = "./page2.html"; 
 }
 
+
 var button = document.getElementById("print");
         button.addEventListener("click", function () {
             var doc = new jsPDF();
             var pdf = document.querySelector(".GFG");
             doc.fromHTML(pdf);
-            doc.save("Recipe.pdf");
+            doc.save("Recipe");
         });
