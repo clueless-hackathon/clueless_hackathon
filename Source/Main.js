3c744bd17464376a094b6670a4b35e31ebe2e278
trina314 <trina0dang@gmail.com>
2023-09-30 14:56:26 -0500

    final

--- Source/Main.js
+++ Source/Main.js
@@ -1,60 +1,20 @@
-var button = document.getElementById("print");
-        button.addEventListener("click", function () {
-            var doc = new jsPDF();
-            var pdf = document.querySelector(".toPrint");
-            doc.fromHTML(pdf);
-            doc.save("Recipe.pdf");
-        });
+const buttons = document.querySelectorAll('.button');
+console.log(buttons)
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Remove the 'select' class from all buttons
+    buttons.forEach(btn => {
+      btn.classList.remove('selected');
+      
+    });
 
+    // Add the 'select' class to the clicked button
+    button.classList.add('selected');
+    });
+});
 
+document.getElementById("create").addEventListener("click", redirectCreate);
 
-        class Recipe {
-            /** A class that defines a Recipe card.
-             * 
-             * @param {String} ti tle 
-             * @param {String} author 
-             * @param {String[]} ingredients 
-             * @param {String} instructions 
-             */
-            constructor(title, author, ingredients, instructions) {
-                this.title = title,
-                this.author = author;
-                this.ingredients = ingredients;
-                this.instructions = instructions;
-            }
-        
-            getTitle() {
-                return this.title;
-            }
-        
-            getAuthor() {
-                return this.author;
-            }
-        
-            getIngredients(){
-                return this.ingredients;
-            }
-        
-            getInstructions(){
-                return this.instructions;
-            }
-        
-            compareTo(other){
-                return this.title.localeCompare(other.title); 
-            }
-        }
-        
-        function addRecipe(recipe){
-            recipeList.push(recipe);
-        }
-        
-        function addRecipe(title, author, ingredients, instructions){
-            recipeList.push(new Recipe(title, author, ingredients, instructions));
-        }
-        
-        var recipeList = [];
-        let button = document.getElementById("button");
-        button.addEventListener("click", function() {
-            let inputs = document.getElementsByClassName("Input");
-            addRecipe(new Recipe(inputs[0], inputs[1], inputs[2], inputs[3]));
-        });
+function redirectCreate(){ 
+    window.location = "./create.html"; 
+}
---
+++ Source/Recipe.js
@@ -0,0 +1,50 @@
+class Recipe {
+    /** A class that defines a Recipe card.
+     * 
+     * @param {String} title 
+     * @param {String} author 
+     * @param {String[]} ingredients 
+     * @param {String} instructions 
+     */
+    constructor(title, author, ingredients, instructions) {
+        this.title = title,
+        this.author = author;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+    }
+
+    getTitle() {
+        return this.title;
+    }
+
+    getAuthor() {
+        return this.author;
+    }
+
+    getIngredients(){
+        return this.ingredients;
+    }
+
+    getInstructions(){
+        return this.instructions;
+    }
+
+    compareTo(other){
+        return this.title.localeCompare(other.title); 
+    }
+}
+
+function addRecipe(recipe){
+    recipeList.push(recipe);
+}
+
+function addRecipe(title, author, ingredients, instructions){
+    recipeList.push(new Recipe(title, author, ingredients, instructions));
+}
+
+var recipeList = [];
+let button = document.getElementById("button");
+button.addEventListener("click", function() {
+    let inputs = document.getElementsByClassName("Input");
+    addRecipe(new Recipe(inputs[0], inputs[1], inputs[2], inputs[3]));
+});
