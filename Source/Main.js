33c2121b05f05db9b13065b1b31a4848b0aa6298
Brandon-Ledet <120496007+Brandon-Ledet@users.noreply.github.com>
2023-09-30 13:23:24 -0500

    Download Recipe inside page2
    Puts the download recipe as PDF function in the recipe page

--- Source/Main.js
+++ Source/Main.js
@@ -1,15 +1,15 @@
+var button = document.getElementById("print");
+        button.addEventListener("click", function () {
+            var doc = new jsPDF();
+            var pdf = document.querySelector(".toPrint");
+            doc.fromHTML(pdf);
+            doc.save("Recipe");
+            doc.save("Recipe.pdf");
+        });
+
 //button that redirects to subpage
 document.getElementById("redirectButton").addEventListener("click", redirect);
 
 function redirect(){ 
     window.location = "./page2.html"; 
 }
-
-
-var button = document.getElementById("print");
-        button.addEventListener("click", function () {
-            var doc = new jsPDF();
-            var pdf = document.querySelector(".GFG");
-            doc.fromHTML(pdf);
-            doc.save("Recipe");
-        });
