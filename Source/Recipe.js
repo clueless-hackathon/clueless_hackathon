7bf2e4f0f36ff4e2646cf25e89bf4c0c109c21bc
TristanD0703 <111524307+TristanD0703@users.noreply.github.com>
2023-09-30 10:33:57 -0500

    oopsies

--- Source/Recipe.js
+++ Source/Recipe.js
@@ -1,1 +1,36 @@
-console.log("Hello, World!");
+var recipeList = [];
+
+
+class Recipe {
+    constructor(title, author, ingredients, instructions) {
+        this.title = title,
+        this.author = author;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+    }
+
+    getTitle() {
+        return this.title;
+    }
+
+    getAuthor() {
+        return this.author;
+    }
+
+    getIngredients(){
+        return this.ingredients;
+    }
+
+    getInstructions(){
+        return this.instructions;
+    }
+}
+
+function addRecipe(recipe){
+    recipeList.push(recipe);
+}
+
+function addRecipe(title, author, ingredients, instructions){
+    recipeList.push(new Recipe(title, author, ingredients, instructions));
+}
+
