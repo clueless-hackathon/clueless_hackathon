2e961184df32e50bb066984d2a83eb7c0ced230a
Andy <77463134+Andy8154@users.noreply.github.com>
2023-09-30 11:25:28 -0500

    Highlight button

--- Source/Main.js
+++ Source/Main.js
@@ -1,1 +1,14 @@
-console.log('Hello!')
+// Get all the buttons and add click event listeners
+const buttons = document.querySelectorAll('.selected');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Remove the 'select' class from all buttons
+    buttons.forEach(btn => {
+      btn.classList.remove('selected');
+    });
+
+    // Add the 'select' class to the clicked button
+    button.classList.add('selected');
+  });
+});
