188171bdb6e5887fc0340ecf95b4d7badf45ecfa
Brandon-Ledet <120496007+Brandon-Ledet@users.noreply.github.com>
2023-09-30 18:54:10 -0500

    Added PDF download to Creation
    Just in Case :)

--- Source/Recipe.js
+++ Source/Recipe.js
@@ -1,24 +1,18 @@
 class Recipe {
     /** A class that defines a Recipe card.
      * 
-     * @param {String} title 
-     * @param {String} author 
+     * @param {String} title  
      * @param {String[]} ingredients 
-     * @param {String} instructions 
+     * @param {String[]} instructions 
      */
-    constructor(title, author, ingredients, instructions) {
+    constructor(title, ingredients, instructions) {
         this.title = title,
-        this.author = author;
         this.ingredients = ingredients;
         this.instructions = instructions;
     }
 
     getTitle() {
         return this.title;
-    }
-
-    getAuthor() {
-        return this.author;
     }
 
     getIngredients(){
@@ -38,13 +32,13 @@ function addRecipe(recipe){
     recipeList.push(recipe);
 }
 
-function addRecipe(title, author, ingredients, instructions){
-    recipeList.push(new Recipe(title, author, ingredients, instructions));
+function addRecipe(title, ingredients, instructions){
+    recipeList.push(new Recipe(title, ingredients, instructions));
 }
 
 var recipeList = [];
 let button = document.getElementById("button");
 button.addEventListener("click", function() {
     let inputs = document.getElementsByClassName("Input");
-    addRecipe(new Recipe(inputs[0], inputs[1], inputs[2], inputs[3]));
+    addRecipe(new Recipe(inputs[0], inputs[1], inputs[2]));
 });
